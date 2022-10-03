118aad91af0bc8952495a8cc8d96a1fd6d0e47b3
Cheliadin <cheliadin.alex@gmail.com>
2022-10-03 15:21:36 +0300

    HYS_FE_ls_4 fixed effects on sliders buttons, footer and menu

--- html_css/mobile-menu.js
+++ html_css/mobile-menu.js
@@ -1,59 +1,76 @@
-window.addEventListener('scroll', function(){
-    const header = document.querySelector('.header');
-    header.classList.toggle('sticky', window.scrollY > 0)
-})
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
-const closeMenu = document.querySelector('.nav__close');
-const hamburger = document.querySelector('.hamburger-lines');
-const navMenu = document.querySelector('.nav__list');
+const closeMenu = document.querySelector(".nav__close");
+const hamburger = document.querySelector(".hamburger-lines");
+const navMenu = document.querySelector(".nav__list");
 
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-    closeMenu.classList.toggle('show');
-})
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  closeMenu.classList.toggle("show");
+});
 
-navMenu.addEventListener('click', function() {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
-    closeMenu.classList.remove('show');
-})
+navMenu.addEventListener("click", function () {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+  closeMenu.classList.remove("show");
+});
 
-closeMenu.addEventListener('click', function() {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
-    closeMenu.classList.remove('show');
-})
+closeMenu.addEventListener("click", function () {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+  closeMenu.classList.remove("show");
+});
 
 function updateWidgetAreaClassList() {
-    if (window.innerWidth >= 768) {
-        navMenu.classList.remove('active');
-    }
+  if (window.innerWidth >= 768) {
+    navMenu.classList.remove("active");
+    closeMenu.classList.remove("show");
+  }
 }
 
-window.addEventListener('resize', updateWidgetAreaClassList, false);
+window.addEventListener("resize", updateWidgetAreaClassList, false);
 
-const allLinks = document.querySelectorAll('.nav__link')
+const allLinks = document.querySelectorAll(".nav__link");
 console.log(allLinks);
 
-navMenu.addEventListener('click', function(event) {
-    allLinks.forEach(link => {
-        link.classList.remove('nav__link-active');
-    }) 
-    event.target.classList.add('nav__link-active');
-})
+navMenu.addEventListener("click", function (event) {
+  allLinks.forEach((link) => {
+    link.classList.remove("nav__link-active");
+  });
+  event.target.classList.add("nav__link-active");
+});
 
-const dots = document.querySelector('.blog__slider-dots')
-const allDots = document.querySelectorAll('.blog__slider-dot')
+const blogDots = document.querySelector(".blog__slider-dots");
+const blogAllDots = document.querySelectorAll(".blog__slider-dot");
 
-dots.addEventListener('click', function(event) {
-    allDots.forEach(link => {
-        link.classList.remove('blog__slider-active');
-    })
-    event.target.classList.add('blog__slider-active');
-})
+blogDots.addEventListener("click", function (event) {
+  const btn = "BUTTON";
+  if (event.target.tagName === btn) {
+    blogAllDots.forEach((link) => {
+      link.classList.remove("blog__slider-active");
+    });
+    event.target.classList.add("blog__slider-active");
+  }
+});
 
-const submit = document.querySelector('.btn__form');
-submit.addEventListener('click', function(event) {
-    event.preventDefault();
-})
+const custDots = document.querySelector(".customers__slider-dots");
+const custAllDots = document.querySelectorAll(".customers__slider-dot");
+
+custDots.addEventListener("click", function (event) {
+  const btn = "BUTTON";
+  if (event.target.tagName === btn) {
+    custAllDots.forEach((link) => {
+      link.classList.remove("customers__slider-active");
+    });
+    event.target.classList.add("customers__slider-active");
+  }
+});
+
+const submit = document.querySelector(".btn__form");
+submit.addEventListener("click", function (event) {
+  event.preventDefault();
+});
