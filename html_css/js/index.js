51f42923b252e2b5c6995948cb34bfc604abb02c
Cheliadin <cheliadin.alex@gmail.com>
2022-10-25 14:30:42 +0300

    HYS_FE_ls_5 added class Storage

--- html_css/js/index.js
+++ html_css/js/index.js
@@ -1,23 +1,26 @@
-import { data } from './data.js';
-import { Slider } from './slider.js';
-import { SliderBlog } from './slider-blog.js';
-import {scrollApp, burger} from './mobile-menu.js';
-import { SliderPref } from './slider-prefer.js';
+import { data } from "./data.js";
+import { Slider } from "./slider.js";
+import { SliderBlog } from "./slider-blog.js";
+import { scrollApp, burger } from "./mobile-menu.js";
+import { SliderPref } from "./slider-prefer.js";
+import { Storage } from "./storage.js";
 
 class App {
-    constructor() {
-        scrollApp();
-        burger('nav__close', 'hamburger-lines', 'nav__list', 'nav__link');
-        
-        let custSlider = new Slider('customers__slider', data);
-        custSlider.init();
-        
-        let blogSlider = new SliderBlog('blog__slider', data);
-        blogSlider.init();
-        
-        let prefSlider = new SliderPref('prefer__slider');
-        prefSlider.init()
-    }
+  constructor() {
+    scrollApp();
+    burger("nav__close", "hamburger-lines", "nav__list", "nav__link");
+
+    let storage = new Storage(data, "dataSlider");
+
+    let custSlider = new Slider("customers__slider", storage.setSliderData());
+    custSlider.init();
+
+    let blogSlider = new SliderBlog("blog__slider", storage.setSliderData());
+    blogSlider.init();
+
+    let prefSlider = new SliderPref("prefer__slider");
+    prefSlider.init();
+  }
 }
 
 new App();
